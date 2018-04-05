f42c22013400254ee3769af8ea8c883c5656d686
Usuário Convidado <Guest@pa06micro03.local>
2018-04-04 21:49:39 -0300

    Initial Commit

---
+++ MoviesLib/WebViewController.swift
@@ -0,0 +1,37 @@
+//
+//  WebViewController.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 04/04/18.
+//  Copyright © 2018 EricBrito. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+    
+    var url: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
